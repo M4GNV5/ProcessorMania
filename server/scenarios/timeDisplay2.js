7a69dedb26515aa31a740838f470ecfb1e3f7cbb
M4GNV5 <eragonjml@googlemail.com>
2016-09-03 00:47:45 +0200

    add 2 player version of the timeDisplay mission

---
+++ server/scenarios/timeDisplay2.js
@@ -0,0 +1,58 @@
+function randomPort()
+{
+	return Math.round(Math.random() * 1000) % 255 + 1;
+}
+
+var cpu = {
+	portToPit: randomPort(),
+	displayPort: randomPort(),
+
+	tickRate: 5,
+	memorySize: 256,
+	modules: ["base", "conditional", "bit", "bcdreg", "alu", "stack"],
+	mainReg: "ax",
+	out: function(port, val)
+	{
+		if(port == this.portToPit)
+			pit.socket.sendJson({cmd: "IO", port: pit.portToCpu, value: val});
+
+		else if(port == this.displayPort)
+		{
+			var display = Math.min(99, val >> 8) + ":" + Math.min(99, val & 0xFF);
+			cpu.socket.sendJson({cmd: "display", text: display});
+			pit.socket.sendJson({cmd: "display", text: display});
+		}
+	}
+};
+var pit = {
+	portToCpu: randomPort(),
+	secPort: randomPort(),
+	milliPort: randomPort(),
+
+	tickRate: 50,
+	memorySize: 0,
+	modules: ["base", "conditional", "bcdreg", "alu"],
+	mainReg: "ax",
+	setup: function()
+	{
+		this.in(this.secPort);
+		this.in(this.milliPort);
+	},
+	in: function(port)
+	{
+		if(port == this.secPort)
+			this.socket.sendJson({cmd: "IO", port: this.secPort, value: (Date.now() / 1000) & 0xFFFF});
+		if(port == this.milliPort)
+			this.socket.sendJson({cmd: "IO", port: this.milliPort, value: Date.now() % 1000});
+	},
+	out: function(port, val)
+	{
+		if(port == this.portToCpu)
+			cpu.socket.sendJson({cmd: "raise", id: val, text: "PIT interrupt"});
+	}
+};
+
+module.exports = [
+	cpu,
+	pit
+];
